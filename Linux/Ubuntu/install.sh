0c6cb3cf425478adc1195972b7bf5c6e209c115a
198sahilaws <98281399+198sahilaws@users.noreply.github.com>
2026-09-24 18:26:51 +0530

    Updated the list of Ubuntu Versions

--- Linux/Ubuntu/install.sh
+++ Linux/Ubuntu/install.sh
@@ -18,7 +18,7 @@ DIR="/opt/zscaler/zms"
 LOG_FILE="/var/log/zscaler_zms_provision.log"
 PROVISION_KEY_FILENAME="provision_key"
 STAGE_DIR=""                                               # Private download staging dir (set at runtime)
-SUPPORTED_VERSIONS="16.04.7 18.04.6 20.04.6 22.04.5 24.04.2 24.04.3 24.04.4 26.04.2"
+SUPPORTED_VERSIONS="16.04 18.04 22.04 24.04 26.04"
 
 #-------------------------------------------------------------------------------
 # Logging
